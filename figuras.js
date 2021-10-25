19e582ff112f94b93acb86c5b86f2d4de86b5af5
Dev <dev@Andrea-Mendis-iMac.local>
2021-10-25 16:47:41 -0500

    Perímetro y área del cuadrado calculadora JS

---
+++ figuras.js
@@ -0,0 +1,11 @@
+// Variables del cuadrado
+//calcular el perímetro de un cuadrado de 5cm
+
+const ladoCuadrado = 5;
+console.log(`Los lados del cuadrado miden: ${ladoCuadrado} cm`);
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log(`El perímetro del cuadrado es: ${perimetroCuadrado} cm`);
+
+const areaCuadrada = ladoCuadrado * ladoCuadrado;
+console.log(`El perímetro del cuadrado es: ${areaCuadrada} cm`);
