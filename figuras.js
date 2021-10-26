b081590fc06352a860a26b855210e1898cd6d44d
Dev <dev@Andrea-Mendis-iMac.local>
2021-10-26 17:00:14 -0500

    se agrega calculadora del triangulo y del circulo

--- figuras.js
+++ figuras.js
@@ -1,11 +1,61 @@
 // Variables del cuadrado
-//calcular el perímetro de un cuadrado de 5cm
 
-const ladoCuadrado = 5;
-console.log(`Los lados del cuadrado miden: ${ladoCuadrado} cm`);
+//Código del cuadrado
+console.group('Cuadrados');
 
-const perimetroCuadrado = ladoCuadrado * 4;
-console.log(`El perímetro del cuadrado es: ${perimetroCuadrado} cm`);
+  const ladoCuadrado = 5;
+  console.log(`Los lados del cuadrado miden: ${ladoCuadrado} cm`);
 
-const areaCuadrada = ladoCuadrado * ladoCuadrado;
-console.log(`El perímetro del cuadrado es: ${areaCuadrada} cm`);
+  const perimetroCuadrado = ladoCuadrado * 4;
+  console.log(`El perímetro del cuadrado es: ${perimetroCuadrado} cm`);
+
+  const areaCuadrado = ladoCuadrado * ladoCuadrado;
+  console.log(`El área del cuadrado es: ${areaCuadrado} cm^2`);
+
+console.groupEnd();
+
+
+
+//Código del triángulo
+
+console.group('Triángulos');
+const ladoTriangulo1 = 5;
+const ladoTriangulo2 = 5;
+const baseTriangulo = 3;
+console.log(`Los lados del triángulo miden: ${ladoTriangulo1} cm, ${ladoTriangulo2} cm y la base ${baseTriangulo} cm`);
+
+const alturaTriangulo = 6.5;
+console.log(`La altura del triángulo es: ${alturaTriangulo} cm`);
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+console.log(`El perímetro del triángulo es: ${perimetroTriangulo} cm`);
+
+const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+console.log(`El área del triángulo es: ${areaTriangulo} cm^2`);
+
+console.groupEnd();
+
+
+
+// Código del círculo
+console.group('Círculos');
+
+// Radio
+const radioCirculo = 5;
+
+// Diámetro
+const diametroCirculo = radioCirculo * 2;
+
+// PI
+// const PI = 3.14159 
+const PI = Math.PI;
+
+// Circunferencia
+const perimetroCirculo = diametroCirculo * PI;
+
+// Área
+const areaCirculo = 
+
+
+
+console.groupEnd();
