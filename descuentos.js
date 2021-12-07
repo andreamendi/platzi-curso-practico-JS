2c4454caa6fb8c16a8dda2c5a08579bc01a11038
Dev <dev@Andrea-Mendis-iMac.local>
2021-12-07 17:28:11 -0600

    Ya se está detectando qué cupón está siendo usado, ahora lo estamos haciendo con SWITCH

--- descuentos.js
+++ descuentos.js
@@ -2,6 +2,12 @@
 // const descuento = 18;
 
 
+const coupons = [
+  'cartasdeunlector',
+  'bookeadora',
+  'leyendoando',
+];
+
 
 
 function calcularPrecioConDescuento(precio, descuento) {
@@ -15,24 +21,38 @@ function onClickButtonPriceDiscount() {
   const inputPrice = document.getElementById("InputPrice");
   const priceValue = inputPrice.value;
 
-  const inputDiscount = document.getElementById("InputDiscount");
-  const discountValue = inputDiscount.value;
+  const inputCoupon = document.getElementById("InputCoupon");
+  const couponValue = inputCoupon.value;
+
+  let descuento;
+
+
+  switch(couponValue) {
+    case coupons[0]: //CartasDeUnLector
+      descuento = 10;
+    break;
+    case coupons[1]: //Bookeadora
+      descuento = 20;
+    break;
+    case coupons[2]: //Leyendoando
+      descuento = 15;
+    break;
+  }
+
 
   const precioConDescuento = calcularPrecioConDescuento(
     priceValue,
-    discountValue
+    descuento
   );
-  const resultP = document.getElementById("priceFinal");
-  resultP.innerHTML = `
-  <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
-  <div class="card-body">
-    <p class="card-text">El precio final ya con descuento es: </p>
-    <h5 class="card-title">${precioConDescuento}$ USD</h5>
-  </div>
-</div>.
 
-  
-`;
+  const resultP = document.getElementById("priceFinal");
+  resultP.innerHTML = `<div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+  <div class="card-body">
+    <h6>Gracias por usar el cupón de ${couponValue} </h6>
+    <p class="card-text">El precio final ya con descuento es: </p>
+    <h4 class="card-title">${precioConDescuento}$ USD</h4>
+  </div>
+</div>`;
 }
 
 // console.log({
