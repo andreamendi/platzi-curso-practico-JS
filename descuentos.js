01078c1e821ff99c36eed8e408ea6bfa37c05387
Dev <dev@Andrea-Mendis-iMac.local>
2021-12-08 17:24:25 -0600

    Se realizó la opción 3 del retor 2, y funcionó genial, se agregó un json, el cual trae información de los descuentos, como el nombre, el código del cupón y el descuento, estos datos de imprimen en el DOM con una serie de funciones, también se quitaron los if's y else's que sobraban

--- descuentos.js
+++ descuentos.js
@@ -1,7 +1,28 @@
 // const precioOriginal = 120;
 // const descuento = 18;
 
-const coupons = ["cartasdeunlector", "bookeadora", "leyendoando"];
+// Opción 1 y 2
+// const coupons = ["cartasdeunlector", "bookeadora", "leyendoando"];
+
+
+// Opción 3
+  const coupons = [
+    {
+      name: 'Cartas de un Lector',
+      code: 'cartasdeunlector',
+      discount: 10,
+    },
+    {
+      name: 'Bookeadora',
+      code: 'bookeadora',
+      discount: 20,
+    },
+    {
+      name: 'Leyendo Ando',
+      code: 'leyendoando',
+      discount: 15,
+    }
+  ];
 
 function calcularPrecioConDescuento(precio, descuento) {
   const porcentajePrecioConDescuento = 100 - descuento;
@@ -18,7 +39,6 @@ function onClickButtonPriceDiscount() {
   const couponValue = inputCoupon.value;
 
   let descuento;
-  let descuentoTexto;
 
   // Opción 1
 
@@ -54,42 +74,83 @@ function onClickButtonPriceDiscount() {
 
 
 
-
   // Opción 2
 
-  if (!coupons.includes(couponValue)) {
-    alert(`El cupón "${couponValue}" no es válido`);
-  } else if (couponValue === "cartasdeunlector") {
-    descuento = 10;
-    console.log('Op2')
-  } else if (couponValue === "bookeadora") {
-    descuento = 20;
-  } else if (couponValue === "leyendoando") {
-    descuento = 15;
+  // if (!coupons.includes(couponValue)) {
+  //   alert(`El cupón "${couponValue}" no es válido`);
+  // } else if (couponValue === "cartasdeunlector") {
+  //   descuento = 10;
+  //   console.log('Op2')
+  // } else if (couponValue === "bookeadora") {
+  //   descuento = 20;
+  // } else if (couponValue === "leyendoando") {
+  //   descuento = 15;
+  // }
+
+  // if (!coupons.includes(couponValue)) {
+  //   const resultP = document.getElementById("priceFinal");
+  //   resultP.innerHTML = `<div class="card text-white bg-warning mb-3" style="max-width: 18rem;">
+  //   <div class="card-body">
+  //     <p class="card-text">El precio final es: </p>
+  //     <h4 class="card-title">${priceValue}$ USD</h4>
+  //   </div>
+  // </div>`;
+  // } else {
+  //   const precioConDescuento = calcularPrecioConDescuento(
+  //     priceValue,
+  //     descuento
+  //   );
+  //   const resultP = document.getElementById("priceFinal");
+  //   resultP.innerHTML = `<div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+  //   <div class="card-body">
+  //     <h6>Gracias por usar el cupón de ${couponValue}</h6>
+  //     <p class="card-text">El precio final ya con descuento es: </p>
+  //     <h4 class="card-title">${precioConDescuento}$ USD</h4>
+  //   </div>
+  // </div>`;
+  // }
+
+
+
+
+  // Opción 3
+
+  const isCouponValueValid = function(coupon) {
+    return coupon.code === couponValue;
   }
 
-  if (!coupons.includes(couponValue)) {
+  const userCoupon = coupons.find(isCouponValueValid)
+
+  if (!userCoupon) {
+    alert(`El cupón "${couponValue}" no es válido`);
     const resultP = document.getElementById("priceFinal");
-    resultP.innerHTML = `<div class="card text-white bg-warning mb-3" style="max-width: 18rem;">
-    <div class="card-body">
-      <p class="card-text">El precio final es: </p>
-      <h4 class="card-title">${priceValue}$ USD</h4>
-    </div>
-  </div>`;
-  } else {
-    const precioConDescuento = calcularPrecioConDescuento(
-      priceValue,
-      descuento
-    );
-    const resultP = document.getElementById("priceFinal");
-    resultP.innerHTML = `<div class="card text-white bg-success mb-3" style="max-width: 18rem;">
-    <div class="card-body">
-      <h6>Gracias por usar el cupón de ${couponValue}</h6>
-      <p class="card-text">El precio final ya con descuento es: </p>
-      <h4 class="card-title">${precioConDescuento}$ USD</h4>
-    </div>
-  </div>`;
+    resultP.innerHTML = `
+    <div class="card text-white bg-warning mb-3" style="max-width: 18rem;">
+      <div class="card-body">
+        <p class="card-text">El precio final es: </p>
+        <h4 class="card-title">${priceValue}$ USD</h4>
+      </div>
+    </div>`;
+  } 
+  else {
+    const descuento = userCoupon.discount;
+    const nombreCupon = userCoupon.name;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+  
+    const resultP = document.getElementById('priceFinal');
+    resultP.innerHTML = `
+    <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+      <div class="card-body">
+        <h6>Gracias por usar el cupón de ${nombreCupon}</h6>
+        <p class="card-text">El precio final ya con descuento es de:</p>
+        <h4 class="card-title">${precioConDescuento}$ USD</h4>
+      </div>
+    </div>`;
+
+    console.log('op3');
   }
+
 
 
 
