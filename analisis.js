dda095b01bb92ccca9321ee6d35e4e52f328870f
Dev <dev@Andrea-Mendis-iMac.local>
2022-03-28 11:39:02 -0600

    Se acabó con la Mediana general, se empieza con la mediana del top 10

--- analisis.js
+++ analisis.js
@@ -1,78 +1,5 @@
-// import { colombia } from "./salarios";
-
-// const salariosCol = colombia.map(
-//   function (personita) {
-//     return personita.salary;
-//   }
-// );
-
-// const salariosColSorted = salariosCol.sort(
-//   function (salaryA, salaryB) {
-//     return salaryA - salaryB;
-//   }
-// );
-
-// function esPar(numerito) {
-//   return (numerito % 2 === 0);
-// }
-
-// function calcularMediaAritmetica(lista) {
-//   const sumaLista = lista.reduce(
-//     function (valorAcumulado = 0, nuevoElemento) {
-//       return valorAcumulado + nuevoElemento;
-//     }
-//   );
-//   const promedioLista = sumaLista / lista.length;
-//   return promedioLista;
-// }
-// function medianaSalarios(lista) {
-//   const mitad = parseInt(lista.length / 2);
-
-//   if (esPar(lista.length)) {
-//     const personitaMitad1 = lista[mitad - 1];
-//     const personitaMitad2 = lista[mitad];
-
-//     const mediana = calcularMediaAritmetica([personitaMitad1, personitaMitad2]);
-//     return mediana;
-//   } else {
-//     const personitaMitad = lista[mitad];
-//     return personitaMitad;
-//   }
-// }
-
-// console.log(
-//   medianaSalarios(`La mediana de los salarios es de: ${salariosColSorted}`)
-// )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Aquí se está haciendo un array con solo los salarios
-const salariosMex = mexico.map(
-  function (persona) {
-      return persona.salary;
-  }
-);
-
-const salariosMexSorted = salariosMex.sort(
-  function (salaryA, salaryB) {
-    // console.log(`La resta del salario ${ salaryA - salaryB}`)
-    return salaryA - salaryB;
-  }
-); 
+// Helpers / Utils
+// Así se les llama a las funciones que no son parte de la lógica del "análisis" pero las necesitamos para los calculos.
 
 function esPar(numero) {
   return (numero % 2 === 0);
@@ -84,33 +11,67 @@ function calcularMediaAritmetica(lista) {
       return valorAcumulado + nuevoElemento;
     });
   const promedioLista = sumaLista / lista.length;
-  console.log(`Es el promedioLista ${promedioLista}`)
+  // console.log(`promedioLista ${promedioLista}`)
   return promedioLista;
 }
 
+
+// Calculadora de Mediana
 function medianaSalarios(lista) {
-  console.log(`Es el mitad ${lista}`)
+  // console.log(`Lista ${lista}`)
 
   const mitad = parseInt(lista.length / 2);
-  console.log(`Es el ista.length ${lista.length}`)
-  console.log(`Es el mitad ${mitad}`)
+  // console.log(`Lista.length ${lista.length}`)
+  // console.log(`Mitad ${mitad}`)
 
   if (esPar(lista.length)) {
     const personaMitad1 = lista[mitad - 1];
     const personaMitad2 = lista[mitad];
-    console.log(`Es el personaMitad2 ${personaMitad2}`)
-    console.log(`Es el personaMitad1 ${personaMitad1}`)
+    // console.log(`personaMitad2 ${personaMitad2}`)
+    // console.log(`personaMitad1 ${personaMitad1}`)
     
     const mediana = calcularMediaAritmetica([personaMitad1, personaMitad2]);
-    console.log(`Es el mediana ${mediana}`)
+    // console.log(`mediana ${mediana}`)
     return mediana;
     
   } else {
     const personaMitad = lista[mitad];
-    console.log(`Es el personaMitad ${personaMitad}`)
+    // console.log(`personaMitad ${personaMitad}`)
     return personaMitad;
   }
 }
-console.log(
-  medianaSalarios(salariosMex)
+
+
+// MEDIANA GENERAL
+// Aquí se está haciendo un array con solo los salarios
+const salaryMex = mexico.map(
+  function (persona) {
+      return persona.salary;
+  }
+);
+
+const salaryMexSorted = salaryMex.sort(
+  function (salaryA, salaryB) {
+    // console.log(`La resta del salario ${ salaryA - salaryB}`)
+    return salaryA - salaryB;
+  }
+); 
+
+
+// Mediana top 10%
+(p  * (90))
+const spliceStart = (salaryMexSorted.length * 90) / 100;
+const spliceCount = salaryMexSorted.length - spliceStart;
+
+
+const salariosMexTop10 = salaryMexSorted.splice(spliceStart, )
+// El métoso "Splice" recibe dos argumentos. La primera posición es dónde queremos hacer el corte. Cuántos pedazos queremos que salgan. 
+
+
+
+const medianaGeneralMex= medianaSalarios(salaryMex)
+console.log({
+  medianaGeneralMex,
+  medianaTop10Mex
+}
 )
